9a38128ab3a1f1911b41ece3bfefd8f8afd0f5e4
John Gozde <john@gozde.ca>
2019-12-28 19:26:25 -0700

    Rename private frame array

--- graphicsframe.h
+++ graphicsframe.h
@@ -1,7 +1,7 @@
 // Full-frame bitmap graphic
 
 // Blinking Eyes
-const byte frame0[5][16] PROGMEM = 
+const byte eyes0[5][16] PROGMEM = 
 {
   {0,1,1,1,1,1,1,0,0,1,1,1,1,1,1,0},
   {1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1},
@@ -10,7 +10,7 @@ const byte frame0[5][16] PROGMEM =
   {0,1,1,1,1,1,0,0,0,0,1,1,1,1,1,0}
 };
 
-const byte frame1[5][16] PROGMEM =
+const byte eyes1[5][16] PROGMEM =
 {
   {0,1,1,1,1,1,1,0,0,1,1,1,1,1,1,0},
   {1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1},
@@ -19,7 +19,7 @@ const byte frame1[5][16] PROGMEM =
   {0,1,1,1,1,1,0,0,0,0,1,1,1,1,1,0}
 };
 
-const byte frame2[5][16] PROGMEM =
+const byte eyes2[5][16] PROGMEM =
 {
   {0,1,1,1,1,1,1,0,0,1,1,1,1,1,1,0},
   {1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1},
@@ -27,7 +27,7 @@ const byte frame2[5][16] PROGMEM =
   {1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1},
   {0,1,1,1,1,1,0,0,0,0,1,1,1,1,1,0}
 };
-const byte frame3[5][16] PROGMEM =
+const byte eyes3[5][16] PROGMEM =
 {
   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
   {0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0},
@@ -37,7 +37,7 @@ const byte frame3[5][16] PROGMEM =
 };
 
 // Diamonds
-const byte frame4[5][16] PROGMEM =
+const byte diamonds0[5][16] PROGMEM =
 {
   //     0 1 2 3 4 5 6 7||7 6 5 4 3 2 1 0
   /*0*/{ 0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0 },
@@ -47,7 +47,7 @@ const byte frame4[5][16] PROGMEM =
   /*4*/{ 0,0,0,1,0,0,0,0, 0,0,0,0,0,0,0,0 }
 };
 
-const byte frame5[5][16] PROGMEM =
+const byte diamonds1[5][16] PROGMEM =
 {
   //     0 1 2 3 4 5 6 7||7 6 5 4 3 2 1 0
   /*0*/{ 0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0 },
@@ -57,7 +57,7 @@ const byte frame5[5][16] PROGMEM =
   /*4*/{ 0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0 }
 };
 
-const byte frame6[5][16] PROGMEM =
+const byte diamonds2[5][16] PROGMEM =
 {
   //     0 1 2 3 4 5 6 7||7 6 5 4 3 2 1 0
   /*0*/{ 0,0,0,0,0,0,0,0, 0,0,0,0,1,0,0,0 },
@@ -67,7 +67,7 @@ const byte frame6[5][16] PROGMEM =
   /*4*/{ 0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0 }
 };
 
-const byte frame7[5][16] PROGMEM =
+const byte diamonds3[5][16] PROGMEM =
 {
   //     0 1 2 3 4 5 6 7||7 6 5 4 3 2 1 0
   /*0*/{ 0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0 },
@@ -77,7 +77,7 @@ const byte frame7[5][16] PROGMEM =
   /*4*/{ 0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0 }
 };
 
-const byte frame8[5][16] PROGMEM =
+const byte diamonds4[5][16] PROGMEM =
 {
   //     0 1 2 3 4 5 6 7||7 6 5 4 3 2 1 0
   /*0*/{ 0,0,0,1,0,0,0,0, 0,0,0,0,0,0,0,0 },
@@ -87,7 +87,7 @@ const byte frame8[5][16] PROGMEM =
   /*4*/{ 0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0 }
 };
 
-const byte frame9[5][16] PROGMEM =
+const byte diamonds5[5][16] PROGMEM =
 {
   //     0 1 2 3 4 5 6 7||7 6 5 4 3 2 1 0
   /*0*/{ 0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0 },
@@ -97,7 +97,7 @@ const byte frame9[5][16] PROGMEM =
   /*4*/{ 0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0 }
 };
 
-const byte frame10[5][16] PROGMEM =
+const byte diamonds6[5][16] PROGMEM =
 {
   //     0 1 2 3 4 5 6 7||7 6 5 4 3 2 1 0
   /*0*/{ 0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0 },
@@ -107,7 +107,7 @@ const byte frame10[5][16] PROGMEM =
   /*4*/{ 0,0,0,0,0,0,0,0, 0,0,0,0,1,0,0,0 }
 };
 
-const byte frame11[5][16] PROGMEM =
+const byte diamonds7[5][16] PROGMEM =
 {
   //     0 1 2 3 4 5 6 7||7 6 5 4 3 2 1 0
   /*0*/{ 0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0 },
@@ -118,18 +118,19 @@ const byte frame11[5][16] PROGMEM =
 };
 
 const byte * const frameArray[] PROGMEM = {
-  frame0[0],
-  frame1[0],
-  frame2[0],
-  frame3[0],
-  frame4[0],
-  frame5[0],
-  frame6[0],
-  frame7[0],
-  frame8[0],
-  frame9[0],
-  frame10[0],
-  frame11[0]
+  eyes0[0], // 0
+  eyes1[0],
+  eyes2[0],
+  eyes3[0], // 3
+
+  diamonds0[0], // 4
+  diamonds1[0],
+  diamonds2[0],
+  diamonds3[0],
+  diamonds4[0],
+  diamonds5[0],
+  diamonds6[0],
+  diamonds7[0] // 11
 };
 /*
 {
